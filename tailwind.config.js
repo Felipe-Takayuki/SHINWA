654820533a51d02a799a7f8ce9a2b8699a14c3b0
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-09-20 12:28:26 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+
+    extend: {
+      fontFamily: {
+        "monomanic": ["Monomaniac One", "sans-serif"],
+        "montaga": ["Montaga", "sans-serif"]
+      },
+      colors: {
+        "lightYellow": '#FFF9D4',
+        "grey": "#B9B9B9"
+      }
+    }
+  },
+  plugins: [],
+}
+
