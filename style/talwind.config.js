f51aeb8383943a10bd965c075ccec910e3b944b5
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2024-09-20 17:54:27 -0300

    add origin page

---
+++ style/talwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+
+      extend: {
+        fontFamily: {
+          "monomanic": ["Monomaniac One", "sans-serif"],
+          "montaga": ["Montaga", "sans-serif"]
+        },
+        colors: {
+          "lightYellow": '#FFF9D4',
+          "grey": "#B9B9B9"
+        }
+      }
+    }
+  }
